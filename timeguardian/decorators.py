9d5b40486baa1a2a6adfeb9202515198c840c932
Aldin Cebo <ceboaldin@gmail.com>
2024-01-16 11:19:46 +0100

    Update TimeGuardian decorators and bump version to 0.0.3
    - Refactored TimeGuardian.measure decorator:
      - Changed parameters from logTimeLimit and logMemoryLimit to elapsed and memory.
      - Improved the decorator's functionality to measure and log execution time and memory usage.
      - Enhanced documentation for clearer understanding of usage.
    
    - Updated the TimeGuardian.monitor decorator:
      - Streamlined exception handling and logging for execution time and memory usage.
      - Revised the parameters and internal logic for better performance monitoring.
    
    - General improvements:
      - Added a comment header to decorators.py for better code organization.
      - Updated setup.py to reflect the new version of the package (0.0.3).
      - Minor clean-up in README.md (removed extraneous lines).
    
    These changes aim to enhance the usability and clarity of the TimeGuardian package, making it more efficient and user-friendly for monitoring function execution in Python applications.

--- timeguardian/decorators.py
+++ timeguardian/decorators.py
@@ -1,3 +1,4 @@
+# decorators.py
 import time
 import logging
 import psutil
@@ -10,18 +11,18 @@ configure_logging()
 
 class TimeGuardian:
     @staticmethod
-    def measure(_func=None, *, name=None, logTimeLimit=None, logMemoryLimit=None):
+    def measure(_func=None, *, name=None, elapsed=True, memory=False):
         """
-        A decorator to measure and log the execution time and memory usage of a function. Logs only if execution time or memory usage exceeds the specified limits.
+        A decorator to measure and log the execution time of a function.
 
         Parameters:
         _func (callable, optional): The function to be decorated.
-        name (str, optional): A custom name to log with the execution time and memory usage.
-        logTimeLimit (int, optional): The execution time limit in milliseconds. Logs only if the execution time exceeds this limit.
-        logMemoryLimit (int, optional): The memory usage limit in bytes. Logs only if the memory usage exceeds this limit.
+        name (str, optional): A custom name to log with the execution time.
+        elapsed (bool, optional): Flag to enable/disable measuring the elapsed time. Default is True.
+        memory (bool, optional): Flag to enable/disable measuring the memory usage. Default is False.
 
         Returns:
-        callable: A wrapper function that adds time and memory measurement functionality.
+        callable: A wrapper function that adds time measurement functionality.
 
         Usage:
         @TimeGuardian.measure
@@ -31,9 +32,54 @@ class TimeGuardian:
         @TimeGuardian.measure(name="CustomName")
         def another_function():
             # function implementation
+        """
+        def decorator(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                if elapsed:
+                    start_time = time.time()
+                if memory:
+                    start_memory = psutil.Process().memory_info().rss
+                try:
+                    result = func(*args, **kwargs)
+                finally:
+                    if memory:
+                        end_memory = psutil.Process().memory_info().rss
+                        memory_usage = end_memory - start_memory
+                        log_message = f'{name} - ' if name else ''
+                        log_message += f'Memory usage: {memory_usage} bytes'
+                        logger.info(log_message)
+                    if elapsed:
+                        end_time = time.time()
+                        elapsed_time = (end_time - start_time) * 1000
+                        log_message = f'{name} - ' if name else ''
+                        log_message += f'Elapsed time: {elapsed_time:.3f}ms'
+                        logger.info(log_message)
+                return result
+            return wrapper
 
-        @TimeGuardian.measure(logTimeLimit=200, logMemoryLimit=1024) # logTimeLimit is in milliseconds, logMemoryLimit is in bytes
-        def limited_function():
+        if _func is None:
+            return decorator
+        else:
+            return decorator(_func)
+
+    @staticmethod
+    def monitor(_func=None, *, name=None, elapsed=None, memory=None):
+        """
+        A decorator to monitor and log the execution time and memory usage of a function. Logs only if execution time or memory usage exceeds the specified limits.
+
+        Parameters:
+        _func (callable, optional): The function to be decorated.
+        name (str, optional): A custom name to log with the execution time and memory usage.
+        elapsed (int, optional): The execution time limit in milliseconds. Logs only if the execution time exceeds this limit.
+        memory (int, optional): The memory usage limit in bytes. Logs only if the memory usage exceeds this limit.
+
+        Returns:
+        callable: A wrapper function that adds time and memory monitoring functionality.
+
+        Usage:
+        @TimeGuardian.monitor(elapsed=200, memory=1024) # elapsed is in milliseconds, memory is in bytes
+        def monitored_function():
             # function implementation
         """
         def decorator(func):
@@ -43,32 +89,21 @@ class TimeGuardian:
                 start_memory = psutil.Process().memory_info().rss
                 try:
                     result = func(*args, **kwargs)
-                except Exception as e:
-                    end_time = time.time()
-                    end_memory = psutil.Process().memory_info().rss
-                    logger.error(f"Exception in {func.__name__}: {e}")
-                    elapsed_time = (end_time - start_time) * 1000
-                    memory_usage = end_memory - start_memory
-                    if logTimeLimit is None or elapsed_time > logTimeLimit:
-                        logger.info(f"Elapsed time until exception: {elapsed_time:.3f}ms")
-                    if logMemoryLimit is None or memory_usage > logMemoryLimit:
-                        logger.info(f"Memory usage until exception: {memory_usage} bytes")
-                    raise
-                else:
+                finally:
                     end_time = time.time()
                     end_memory = psutil.Process().memory_info().rss
                     elapsed_time = (end_time - start_time) * 1000
                     memory_usage = end_memory - start_memory
-                    if logTimeLimit is None or elapsed_time > logTimeLimit:
+                    if elapsed is not None and elapsed_time > elapsed:
                         log_message = f'{name} - ' if name else ''
                         log_message += f'Elapsed time: {elapsed_time:.3f}ms'
                         logger.info(log_message)
-                    if logMemoryLimit is None or memory_usage > logMemoryLimit:
+                    if memory is not None and memory_usage > memory:
                         log_message = f'{name} - ' if name else ''
                         log_message += f'Memory usage: {memory_usage} bytes'
                         logger.info(log_message)
 
-                    return result
+                return result
             return wrapper
 
         if _func is None:
